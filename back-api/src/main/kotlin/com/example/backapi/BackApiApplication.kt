810ea7b1414cce8bb72cdf54c30e1676434f4835
Rafael Lima <rafaelmoreiralimap4@gmail.com>
2023-08-28 15:36:41 -0300

    first commit

---
+++ back-api/src/main/kotlin/com/example/backapi/BackApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.backapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackApiApplication>(*args)
+}
---
+++ back-api/src/test/kotlin/com/example/backapi/BackApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.backapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
